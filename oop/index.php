cbaf0cb322034245dc9c496072069a0db635c7bd
Edwin Diradinata <edwindiradinata@gmail.com>
2023-04-01 11:15:54 +0700

    meeting 5

---
+++ oop/index.php
@@ -0,0 +1,20 @@
+<?php
+
+class Animal {
+    //property
+    protected $name;
+
+    //method perkenalanan
+    //method skill
+}
+
+class Cat extends Animal {
+    private $skill = 'berlari';
+
+    //method skill
+}
+
+
+$cat = new Cat;
+$cat->name = 'Yuru';
+// panggil method intriodur
